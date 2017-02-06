52fb7da097d9577a504dd206cccf17511aa33fd9
danielmoonlay <daniel.nababan@moonlay.com>
2017-02-06 18:52:42 +0700

    update sql helper

--- src/sql-helper.js
+++ src/sql-helper.js
@@ -1,23 +1,25 @@
-
-var sql = require("mssql");
-
-var config = {
-    server: process.env.SQL_SERVER,
-    database: process.env.SQL_DATABASE,
-    user: process.env.SQL_USERNAME,
-    password: process.env.SQL_PASSWORD,
-    options: {
-        encrypt: true
-    },
-    connectionTimeout: 300 * 60 * 1000,
-    requestTimeout: 60 * 60 * 1000
-
-};
-
 module.exports = class SqlConnection {
+
+    constructor() {
+        this.sql = require("mssql");
+
+        this.config = {
+            server: process.env.SQL_SERVER,
+            database: process.env.SQL_DATABASE,
+            user: process.env.SQL_USERNAME,
+            password: process.env.SQL_PASSWORD,
+            options: {
+                encrypt: true
+            },
+            connectionTimeout: 300 * 60 * 1000,
+            requestTimeout: 60 * 60 * 1000
+
+        };
+    }
+
     startConnection() {
         return new Promise((resolve, reject) => {
-            sql.connect(config, function (err) {
+            this.sql.connect(this.config, function (err) {
                 if (err)
                     reject(err);
                 resolve(true);
@@ -26,10 +28,10 @@ module.exports = class SqlConnection {
     }
 
     transaction() {
-        return new sql.Transaction();
+        return new this.sql.Transaction();
     }
 
     transactionRequest(transaction) {
-        return new sql.Request(transaction);
+        return new this.sql.Request(transaction);
     }
 }
