3c5f6b169a90231601dd5b9c917626707c663120
AchmadSetiaji <achmad.setiaji@moonlay.com>
2017-08-07 15:17:26 +0700

    update core api for etl fact penjualan dan fact penjualan summary

--- src/routers/v1/etl/etl-fact-penjualan-summary-router.js
+++ src/routers/v1/etl/etl-fact-penjualan-summary-router.js
@@ -32,4 +32,30 @@ router.get('/', (request, response, next) => {
         });
 });
 
+router.get('/:startDate/:endDate', (request, response, next) => {
+
+    var mongoDbConnection = db.get();
+    // var sql = new SqlHelper();
+    var sqlConnection = SqlHelper.startConnection();
+    var startDate = request.params.startDate;
+    var endDate = request.params.endDate;
+
+    Promise.all([mongoDbConnection, sqlConnection])
+        .then(result => {
+            var _db = result[0];
+            var instance = new FactPenjualanSummary(_db, {
+                username: "etl"
+            }, SqlHelper, startDate, endDate);
+
+            instance.run()
+                .then(() => {
+                    response.send(200);
+                })
+                .catch(e => {
+                    var error = resultFormatter.fail(apiVersion, 400, e);
+                    response.send(400, error);
+                });
+        });
+});
+
 module.exports = router;
